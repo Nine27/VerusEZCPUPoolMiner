6f44cf189832feb3b8eb4151c8a2607fa42d4823
John Westbrook <ethadvisor@gmail.com>
2019-01-19 00:52:10 -0800

    Add files via upload

---
+++ VerusEZCPUPoolMiner/VerusEZCPUPoolMiner.Designer.vb
@@ -0,0 +1,268 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class VerusEZCPUPoolMiner
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(VerusEZCPUPoolMiner))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.AddressField = New System.Windows.Forms.TextBox()
+        Me.Threads = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.MinerConsole = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ServerField = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.ShowCMD = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.WorkerField = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(8, 95)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(195, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter your Verus Address:"
+        '
+        'AddressField
+        '
+        Me.AddressField.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AddressField.Location = New System.Drawing.Point(12, 118)
+        Me.AddressField.Name = "AddressField"
+        Me.AddressField.Size = New System.Drawing.Size(348, 26)
+        Me.AddressField.TabIndex = 2
+        '
+        'Threads
+        '
+        Me.Threads.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Threads.Location = New System.Drawing.Point(12, 302)
+        Me.Threads.Name = "Threads"
+        Me.Threads.Size = New System.Drawing.Size(71, 26)
+        Me.Threads.TabIndex = 2
+        Me.Threads.Text = "1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(8, 279)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(108, 20)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "CPU Threads:"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(18, 357)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(155, 53)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Start Mining!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(363, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(172, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Miner Output (May not be realtime):"
+        '
+        'MinerConsole
+        '
+        Me.MinerConsole.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MinerConsole.Location = New System.Drawing.Point(366, 25)
+        Me.MinerConsole.Multiline = True
+        Me.MinerConsole.Name = "MinerConsole"
+        Me.MinerConsole.Size = New System.Drawing.Size(597, 385)
+        Me.MinerConsole.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(588, 417)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(269, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "developed with love for the Verus community by Nine27"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(863, 417)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(100, 13)
+        Me.LinkLabel1.TabIndex = 10
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "https://veruscoin.io"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "Verus EZ CPU Pool Miner"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ServerField
+        '
+        Me.ServerField.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ServerField.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.ServerField.Location = New System.Drawing.Point(12, 241)
+        Me.ServerField.Name = "ServerField"
+        Me.ServerField.Size = New System.Drawing.Size(348, 26)
+        Me.ServerField.TabIndex = 13
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(8, 218)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(88, 20)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Server:Port"
+        '
+        'ShowCMD
+        '
+        Me.ShowCMD.AutoSize = True
+        Me.ShowCMD.Location = New System.Drawing.Point(118, 298)
+        Me.ShowCMD.Name = "ShowCMD"
+        Me.ShowCMD.Size = New System.Drawing.Size(172, 30)
+        Me.ShowCMD.TabIndex = 15
+        Me.ShowCMD.Text = "Show CMD window" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(Output will not be shown here)"
+        Me.ShowCMD.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(191, 357)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(155, 53)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "Stop Mining"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'WorkerField
+        '
+        Me.WorkerField.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.WorkerField.Location = New System.Drawing.Point(12, 180)
+        Me.WorkerField.Name = "WorkerField"
+        Me.WorkerField.Size = New System.Drawing.Size(348, 26)
+        Me.WorkerField.TabIndex = 18
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(8, 157)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(257, 20)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Enter your Worker Name (optional):"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Calibri", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(6, 25)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(316, 36)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = "Verus EZ CPU Pool Miner"
+        '
+        'Label6
+        '
+        Me.Label6.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(315, 40)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(36, 16)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "v0.4"
+        '
+        'VerusEZCPUPoolMiner
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(975, 439)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Threads)
+        Me.Controls.Add(Me.WorkerField)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ShowCMD)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.ServerField)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.MinerConsole)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.AddressField)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "VerusEZCPUPoolMiner"
+        Me.Text = "Verus EZ CPU Pool Miner nheqminer wrapper"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents AddressField As TextBox
+    Friend WithEvents Threads As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents MinerConsole As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents ServerField As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents ShowCMD As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents WorkerField As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ VerusEZCPUPoolMiner/VerusEZCPUPoolMiner.vb
@@ -0,0 +1,210 @@
+﻿Public Class VerusEZCPUPoolMiner
+    Private strResults As String
+    Private intStop As Integer
+    Private swReader As System.IO.StreamReader
+    Private thrdCMD As System.Threading.Thread
+    Private Delegate Sub Update()
+    Private uFin As New Update(AddressOf UpdateText)
+    Private Server As String = ""
+    Private Address As String = ""
+    Private Worker As String = ""
+    Private ThreadsAndBlocksCommand As String = ""
+    Private CPUThreads As Integer = 0
+
+    Private Sub UpdateText()
+        MinerConsole.Text += strResults
+        MinerConsole.SelectionStart = MinerConsole.TextLength - 1
+        MinerConsole.Focus()
+        intStop = MinerConsole.SelectionStart
+        MinerConsole.ScrollToCaret()
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ValidationError As Boolean = False
+        Dim ErrorMessage As String = "The Following Errors were found:" & vbNewLine
+        If String.IsNullOrEmpty(AddressField.Text) = False Then
+            Address = AddressField.Text
+        Else
+            ValidationError = True
+            ErrorMessage += "-Address field is empty" & vbNewLine
+        End If
+        If String.IsNullOrEmpty(WorkerField.Text) = False Then
+            Worker = "." & WorkerField.Text
+        Else
+            Worker = ""
+        End If
+        If String.IsNullOrEmpty(ServerField.Text) = False Then
+            Server = ServerField.Text
+        Else
+            ValidationError = True
+            ErrorMessage += "-Server field is empty" & vbNewLine
+        End If
+        If String.IsNullOrEmpty(Threads.Text) = False Then
+                If IsNumeric(Threads.Text) Then
+                    If Threads.Text = "0" = False Then
+                        CPUThreads = Threads.Text
+                    Else
+                        ValidationError = True
+                        ErrorMessage += "-CPU Threads cannot be zero" & vbNewLine
+                    End If
+                Else
+                    ValidationError = True
+                    ErrorMessage += "-CPU Threads is not numeric" & vbNewLine
+                End If
+            Else
+                ValidationError = True
+                ErrorMessage += "-CPU Threads is empty" & vbNewLine
+            End If
+            If ValidationError = True Then
+            MessageBox.Show(ErrorMessage & "Please fix these errors in order to start mining Verus")
+        Else
+            Dim writer As New System.IO.StreamWriter("config.conf", False)
+            writer.WriteLine("Address=" & Address)
+            writer.WriteLine("Worker=" & Worker)
+            writer.WriteLine("Server=" & Server)
+            writer.WriteLine("CPUThreads=" & CPUThreads)
+            writer.WriteLine("ShowCMD=" & ShowCMD.Checked)
+            writer.Close()
+            thrdCMD = New System.Threading.Thread(AddressOf Miner)
+            thrdCMD.IsBackground = True
+            thrdCMD.Start()
+            Button2.Enabled = True
+            Button1.Enabled = False
+        End If
+    End Sub
+    Private Sub Miner()
+        Dim ArgumentsToUse As String = " -v " & "-l " & Server & " -u " & Address & Worker & " -t " & CPUThreads
+        Dim procMiner As New Process
+        If ShowCMD.Checked Then
+            procMiner.StartInfo.RedirectStandardOutput = False
+            procMiner.StartInfo.CreateNoWindow = False
+            procMiner.StartInfo.UseShellExecute = True
+            procMiner.StartInfo.FileName = System.IO.Directory.GetCurrentDirectory & "\nheqminer.exe"
+            procMiner.StartInfo.Arguments = ArgumentsToUse
+            procMiner.StartInfo.WorkingDirectory = System.IO.Directory.GetCurrentDirectory & "\"
+            procMiner.Start()
+        Else
+            AddHandler procMiner.OutputDataReceived, AddressOf MinerOutput
+            procMiner.StartInfo.RedirectStandardOutput = True
+            procMiner.StartInfo.CreateNoWindow = True
+            procMiner.StartInfo.UseShellExecute = False
+            procMiner.StartInfo.FileName = System.IO.Directory.GetCurrentDirectory & "\nheqminer.exe"
+            procMiner.StartInfo.Arguments = ArgumentsToUse
+            procMiner.StartInfo.WorkingDirectory = System.IO.Directory.GetCurrentDirectory & "\"
+            procMiner.Start()
+            procMiner.BeginOutputReadLine()
+            Do Until (procMiner.HasExited)
+            Loop
+            procMiner.Dispose()
+        End If
+    End Sub
+    Private Sub MinerOutput(ByVal Sender As Object, ByVal OutputLine As DataReceivedEventArgs)
+        strResults = OutputLine.Data & Environment.NewLine
+        Try
+            Invoke(uFin)
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub VerusEZCPUPoolMiner_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+        Dim line As String
+        Dim getdata As String()
+        Dim result As String
+        If My.Computer.FileSystem.FileExists(System.IO.Directory.GetCurrentDirectory & "\nheqminer.exe") = False Then
+            MessageBox.Show("nheqminer not found. Please download the miner and put it inside a folder called ""nheqminer""")
+            Process.Start("https://github.com/VerusCoin/nheqminer/releases")
+            Me.Close()
+        End If
+        Try
+            Using reader As New System.IO.StreamReader("config.conf")
+                While Not reader.EndOfStream
+                    line = reader.ReadLine()
+                    If line.Contains("Address") Then
+                        getdata = line.Split("=")
+                        result = getdata(1)
+                        AddressField.Text = result
+                    ElseIf line.Contains("Server") Then
+                        getdata = line.Split("=")
+                        result = getdata(1)
+                        If result = "usa" Or result = "eu" Then
+                            ServerField.Text = ""
+                        Else
+                            ServerField.Text = result
+                        End If
+                    ElseIf line.Contains("CPUThreads") Then
+                        getdata = line.Split("=")
+                        result = getdata(1)
+                        Threads.Text = result
+                    ElseIf line.Contains("ShowCMD") Then
+                        getdata = line.Split("=")
+                        result = getdata(1)
+                        If result = "True" Then
+                            ShowCMD.Checked = True
+                        Else
+                            ShowCMD.Checked = False
+                        End If
+                    End If
+                End While
+            End Using
+        Catch ex As Exception
+            ServerField.Text = ""
+        End Try
+    End Sub
+
+    Private Sub VerusEZCPUPoolMiner_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        For Each P As Process In System.Diagnostics.Process.GetProcessesByName("nheqminer")
+            P.Kill()
+        Next
+    End Sub
+
+    Private Sub VerusEZCPUPoolMiner_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            NotifyIcon1.Visible = True
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            Me.Hide()
+            NotifyIcon1.BalloonTipTitle = "Verus EZ CPU Pool Miner"
+            NotifyIcon1.BalloonTipText = "Verus EZ CPU Pool Miner running on System Tray"
+            NotifyIcon1.ShowBalloonTip(5000)
+        End If
+    End Sub
+    Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.DoubleClick
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+        NotifyIcon1.Visible = False
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://veruscoin.io")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        For Each P As Process In System.Diagnostics.Process.GetProcessesByName("nheqminer")
+            P.Kill()
+        Next
+        Button1.Enabled = True
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) 
+
+    End Sub
+
+    Private Sub GroupBox3_Enter(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Threads_TextChanged(sender As Object, e As EventArgs) Handles Threads.TextChanged
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+End Class
